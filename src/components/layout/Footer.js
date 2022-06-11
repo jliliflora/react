770bb0c949251bc3660a3d88fcd2192730909249
jliliflora <61509359+jliliflora@users.noreply.github.com>
2022-06-11 14:15:49 +0900

    update 0611 / layout

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,11 +5,11 @@ function Footer(props) {
     <footer id="footer" className={`${props.color}`}>
       <div>
         <h4>email</h4>
-        <a href="mailto:webstroyboy@naver.com">webstoryboy@naver.com</a>
+        <a href="mailto:syon704@gmail.com">syon704@gmail.com</a>
       </div>
       <div>
         <h4>Kakao</h4>
-        <a href="/">webstupis</a>
+        <a href="/">yon704</a>
       </div>
       <div>
         <h4>social</h4>
--- src/components/layout/Title.js
+++ src/components/layout/Title.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Title(props) {
   return (
-    <section className={"cont__title" + `${props.color}`} >
-    {/* <section className="cont__title"> */}
+    // <section className={`cont__title ${props.color}`} >
+    <section className="cont__title">
       <div className="container">
         <h1>
           <strong>{props.title[0]}</strong>
