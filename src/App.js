f0e834dc219812ebfa50d6ffeedbc92d668c7d1c
jliliflora <61509359+jliliflora@users.noreply.github.com>
2022-06-11 14:12:06 +0900

    update 0611 / src

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./components/pages/Main";
 import About from "./components/pages/About";
 import Reference from "./components/pages/Reference";
+import ReferDetail from "./components/pages/ReferDetail";
 import Youtube from "./components/pages/Youtube";
 import Movie from "./components/pages/Movie";
 import Contact from "./components/pages/Contact";
 import Portfolio from "./components/pages/Portfolio";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/reference" element={<Reference />} />
-        <Route path="/youtube" element={<Youtube />} />
-        <Route path="/movie" element={<Movie />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <Route path="/" exact component={Main} />
+      <Route path="/about" exact component={About} />
+      <Route path="/reference" exact component={Reference} />
+      <Route path="/refer-detail" exact component={ReferDetail} />
+      <Route path="/movie" exact component={Movie} />
+      <Route path="/youtube" exact component={Youtube} />
+      <Route path="/contact" exact component={Contact} />
+      <Route path="/portfolio" exact component={Portfolio} />
+    </Router>
   );
 }
 
